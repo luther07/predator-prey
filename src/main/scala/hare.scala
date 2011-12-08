a75b78f5c4f2768df60e4ab7325e3289a656432d
Mark Johnson <luther07@gmail.com>
2011-12-08 01:06:05 -0600

    added case objects for Reproduce, NaturalDeath, PredatorDeath

--- src/main/scala/hare.scala
+++ src/main/scala/hare.scala
@@ -3,6 +3,10 @@ import akka.actor.Actor
 import akka.actor.Actor._
 import scala.util.Random
 
+case object Reproduce
+case object NaturalDeath
+case object PredatorDeath
+
 class Hare extends Actor {
    private val random = new Random()
 
