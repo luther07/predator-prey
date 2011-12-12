e98ede741788d64c9928be2835e1ed617ceda3bb
Mark Johnson <luther07@gmail.com>
2011-12-12 07:17:29 -0600

    moved data structures and functionality from predatorprey-simulator to world; this data and functionality needs to be local to world

--- src/main/scala/predatorprey-simulator.scala
+++ src/main/scala/predatorprey-simulator.scala
@@ -3,7 +3,6 @@ package predatorprey
 import akka.actor.Actor
 import akka.actor.Actor._
 import akka.actor.{PoisonPill}
-import scala.collection.{immutable, mutable}
 import scala.util.Random
 
 /* 
@@ -14,46 +13,12 @@ import scala.util.Random
 
 object PredatorPreySimulator {
   private val random = new Random()
-  private val hares = new mutable.ArrayBuffer[akka.actor.ActorRef]()
-  private val lynxs = new mutable.ArrayBuffer[akka.actor.ActorRef]()
+  //maybe pass in some arguments to World actor?
   private val world = actorOf(new World)
-
-  def generateHares(n: Int) {
-    for (i <- 1 to n) {
-      val hare = actorOf(new Hare(i))
-      hare.start()
-      hares += hare
-    }
-
-    println("[!] generated " + hares.size + " hares")
-  }
-
-  def generateLynxs(n: Int) {
-    for (i <- 1 to n){
-      val lynx = actorOf(new Lynx(i))
-      lynx.start()
-      lynxs += lynx
-    }
-
-    println("[!] generated " + lynxs.size + " lynxs")
-  }
-
-  def shutdownHare(n: Int) {
-    hares(n) ! PoisonPill
-  }
-
-  def isAliveHare() {
-    hares.map(_ ! Alive)
-    }
 
   def main(args: Array[String]) {
     println("[!] starting predator-prey simulation")
     world.start()
-
-  generateHares(20)
-  generateLynxs(20)
-  shutdownHare(0)
-  isAliveHare()
 
   System.exit(0)
   }
--- src/main/scala/world.scala
+++ src/main/scala/world.scala
@@ -1,14 +1,49 @@
 package predatorprey
 import akka.actor.Actor
 import akka.actor.Actor._
+import scala.collection.{immutable, mutable}
+import akka.actor.{PoisonPill}
 import scala.util.Random
 
 case object Time
 
 class World extends Actor {
    private val random = new Random()
+   private val hares = new mutable.ArrayBuffer[akka.actor.ActorRef]()
+   private val lynxs = new mutable.ArrayBuffer[akka.actor.ActorRef]()
+
+   override def preStart {
+      generateHares(20)
+      generateLynxs(20)   
+   }      
 
    def receive = {
       case Time => println("The time is...")   
+   }
+
+   def generateHares(n: Int) {
+      for(i <- 1 to n) {
+         val hare = actorOf(new Hare(i))
+         hare.start()
+         hares += hare
+      }
+      println("[!!] generated " + hares.size + " hares")
+   }
+
+   def generateLynxs(n: Int) {
+      for(i <- 1 to n) {
+         val lynx = actorOf(new Lynx(i))
+         lynx.start()
+         lynxs += lynx
+      }
+      println("[!!] generated " + hares.size + " lynxes")
+   }
+
+   def shutdownHare(n: Int) {
+      hares(n) ! PoisonPill
+   }
+
+   def isAliveHare() {
+      hares.map(_ ! Alive)
    }
 }
