f52a926d5eef4fb01634eceef183a2f5494b912c
George Vasels <vaselsg@gmail.com>
2011-12-14 23:56:14 -0600

    Added CFG

---
+++ src/main/scala/WorldConfiguration.scala
@@ -0,0 +1,14 @@
+package main.scala
+
+object WorldConfiguration {
+    val worldWidth : Int = 800
+	val worldHeight : Int = 800
+	val initialHares : Int = 100
+	val initialLynx : Int = 100
+	val hareBirthRate : Double = .5
+	val maxHareAge : Int = 15
+	val lynxEnergyToReproduce = 10
+	val energyPerEatenHare = 40
+	val maxLynxAge : Int = 15
+    
+}
