9a3f29a3048b53201b4347ef49c98024d9b89759
Mark Johnson <luther07@gmail.com>
2011-12-16 03:46:30 -0600

    simple, isolated unit tests

---
+++ src/test/scala/UnitTestLynx.scala
@@ -0,0 +1,22 @@
+package predatorprey
+import akka.testkit.TestActorRef
+import akka.testkit._
+import akka.util.duration._
+
+class LynxTestActor extends TestKit {
+val MillisTime: Long = 100
+
+  val lynxRef = TestActorRef[Hare].start()
+  val lynxActor = lynxRef.underlyingActor
+
+  within (1 second) {
+    lynxRef ! Alive
+    expectMsg(AliveTrue)
+    expectMsg(Time)
+  }
+
+  within (1 second) {
+    lynxRef ! DateOfBirth(MillisTime)
+    expectMsg(Time)
+  }
+}
